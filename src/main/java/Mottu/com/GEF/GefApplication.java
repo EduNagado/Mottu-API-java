4400dcb7feff1566031c17805605b5340ecd4b71
edunagado <eduardo.nagado@gmail.com>
2025-05-18 15:12:30 -0300

    feat: Adicionando as dependencias

---
+++ src/main/java/Mottu/com/GEF/GefApplication.java
@@ -0,0 +1,13 @@
+package Mottu.com.GEF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Mottu/com/GEF/GefApplicationTests.java
@@ -0,0 +1,13 @@
+package Mottu.com.GEF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
